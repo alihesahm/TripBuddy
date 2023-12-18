62418175c2f251a3b3b8b1f7ff47deca36b4aecc
ali <ali.hesham@gmil.com>
2023-12-18 13:56:00 +0300

    update places migration

--- database/migrations/2023_12_04_092907_create_places_table.php
+++ database/migrations/2023_12_04_092907_create_places_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained();
             $table->foreignId('category_id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('phone');
             $table->string('location');
             $table->string('type');
